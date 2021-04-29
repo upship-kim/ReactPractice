8a9f6ffc068cb0fd1005485c0c2e2806f55e75fd
upship-kim <tkdqook@gmail.com>
2021-04-29 22:48:32 +0900

    create immer library test form2

--- src/immerLibrary/ImmerApp.js
+++ src/immerLibrary/ImmerApp.js
@@ -8,6 +8,11 @@ const ImmerApp = () => {
         uselessValue: null,
     });
 
+    const initState = {
+        name: '',
+        username: '',
+    };
+
     //input 수정을 위한 함수
     const onChange = useCallback(
         (e) => {
@@ -23,12 +28,29 @@ const ImmerApp = () => {
     const onSubmit = useCallback(
         (e) => {
             e.preventDefault();
+            const info = {
+                id: nextId.current,
+                name: form.name,
+                username: form.username,
+            };
             setData({
                 ...data,
-                array: data.array.concat({...form}),
+                array: data.array.concat(info),
             });
+            nextId.current += 1;
+            setForm(initState);
         },
         [data, form]
+    );
+
+    const onRemove = useCallback(
+        (id) => {
+            setData({
+                ...data,
+                array: data.array.filter((form) => form.id !== id),
+            });
+        },
+        [data]
     );
 
     return (
@@ -49,8 +71,8 @@ const ImmerApp = () => {
                 <button type="submit">등록</button>
             </form>
             <ul>
-                {data.array.map((form, index) => (
-                    <li key={index}>
+                {data.array.map((form) => (
+                    <li key={form.id} onClick={() => onRemove(form.id)}>
                         {form.username}({form.name})
                     </li>
                 ))}
