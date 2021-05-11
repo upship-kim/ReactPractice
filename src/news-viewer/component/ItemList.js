099843837ea5900263651d9102dafd2af6f1cd58
upship-kim <tkdqook@gmail.com>
2021-05-11 17:33:03 +0900

    delete custom hook

--- src/news-viewer/component/ItemList.js
+++ src/news-viewer/component/ItemList.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import {apiKey} from '../config/ApiKey';
@@ -12,21 +12,43 @@ const ItemListBlock = styled(Responsive)`
 `;
 
 const ItemList = ({category}) => {
-    const [response, error, loading] = usePromise(() => {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        return axios.get(
-            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${apiKey}`
-        );
-    }, [category]);
-    console.log(category);
-    console.log(response);
-    // eslint-disable-next-line no-unused-vars
-    const {articles} = response.data;
+    //     const [response, error, loading] = usePromise(() => {
+    //         const query = category === 'all' ? '' : `&category=${category}`;
+    //         return axios.get(
+    //             `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${apiKey}`
+    //         );
+    //     }, [category]);
+    //     console.log(category);
+    //     console.log(response);
+    //     const {articles} = response.data;
 
-    if (error) {
-        return <ItemListBlock>data null</ItemListBlock>;
-    }
-    if (!response) {
+    const [article, setArticle] = useState(null);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const query = category === 'all' ? '' : `&category=${category}`;
+                const result = await axios.get(
+                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${apiKey}`
+                );
+                setArticle(result.data.articles);
+                console.log(article);
+            } catch (e) {
+                return (
+                    <ItemListBlock>
+                        <Item> error</Item>
+                    </ItemListBlock>
+                );
+            }
+
+            setLoading(false);
+        };
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [category]);
+
+    if (!article) {
         return null;
     }
     if (loading) {
@@ -34,7 +56,7 @@ const ItemList = ({category}) => {
     }
     return (
         <ItemListBlock>
-            {articles.map((article) => (
+            {article.map((article) => (
                 <Item article={article} key={article.title} />
             ))}
         </ItemListBlock>
