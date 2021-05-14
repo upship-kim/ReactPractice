cf4a8cb19387c2c95cfa81899f4406c879b8666e
upship-kim <tkdqook@gmail.com>
2021-05-14 20:22:31 +0900

    working index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'; //HTML5 History api를 사용해서 현재 주소와 관련된 정보를 props로 쉽게 조회하거나 사용하게 해줌(history, match, location)
+import {combineReducers, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import counter from '../src/redux/module/counter';
+
+const rootReducer = combineReducers({});
+
+const store = createStore();
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
 
