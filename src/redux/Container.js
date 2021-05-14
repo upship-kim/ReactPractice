d119ff8cd3f444a4f6c1d312f940fe41f907b869
upship-kim <tkdqook@gmail.com>
2021-05-14 20:17:52 +0900

    create reducer & container

--- src/redux/Container.js
+++ src/redux/Container.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import Component from './Component';
-
-const dispatch = useDispatch(); 
-
+import {decrease, increase} from './module/counter';
 
 const Container = () => {
-    return <Component increase={} decrease={} number={}/>;
+    const dispatch = useDispatch();
+
+    const number = useSelector((state) => state.counter.number);
+    const onIncrease = () => {
+        dispatch(increase);
+    };
+    const onDecrease = () => {
+        dispatch(decrease);
+    };
+    return (
+        <Component
+            increase={onIncrease}
+            decrease={onDecrease}
+            number={number}
+        />
+    );
 };
 
 export default Container;
