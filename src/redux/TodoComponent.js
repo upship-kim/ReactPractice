5c43274dbf82da40cd172cc9354740ea7a85ef13
upship-kim <tkdqook@gmail.com>
2021-05-17 14:10:45 +0900

    todo list success with redux

--- src/redux/TodoComponent.js
+++ src/redux/TodoComponent.js
@@ -8,27 +8,44 @@ const TodoItem = ({todos, onToggle, onRemove}) => {
                 <div key={todo.id}>
                     <input
                         type="checkBox"
-                        onClick={onToggle}
+                        onClick={() => onToggle(todo.id)}
                         checked={todo.done}
                         readOnly={true}
                     />
-                    <span>{todo.text}</span>
-                    <button onClick={onRemove}>삭제</button>
+                    <span
+                        style={{
+                            textDecoration: todo.done ? 'line-through' : 'none',
+                        }}
+                    >
+                        {todo.text}
+                    </span>
+                    <button onClick={() => onRemove(todo.id)}>삭제</button>
                 </div>
             ))}
         </>
     );
 };
 
-const TodoComponent = ({input, todos, onToggle, onRemove, onInsert}) => {
+const TodoComponent = ({
+    input,
+    todos,
+    onChangeInput,
+    onToggle,
+    onRemove,
+    onInsert,
+}) => {
     const onSubmit = (e) => {
         e.preventDefault();
-        onInsert();
+        onInsert(input);
+        onChangeInput('');
+    };
+    const onChange = (e) => {
+        onChangeInput(e.target.value);
     };
     return (
         <>
             <form onSubmit={onSubmit}>
-                <input />
+                <input value={input} onChange={onChange} />
                 <button type="submit">등록</button>
             </form>
             <TodoItem onToggle={onToggle} onRemove={onRemove} todos={todos} />
--- src/redux/TodoContainer.js
+++ src/redux/TodoContainer.js
@@ -1,31 +1,48 @@
 import React, {useCallback} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {insert, remove, toggle} from './module/todo';
+import {changeInput, insert, remove, toggle} from './module/todo';
 import TodoComponent from './TodoComponent';
 
 const TodoContainer = () => {
     const {input, todos} = useSelector((state) => state.todo);
     const dispatch = useDispatch();
 
-    const onInsert = useCallback(() => {
-        dispatch(insert());
-    }, [dispatch]);
-    const onToggle = useCallback(() => {
-        dispatch(toggle());
-    }, [dispatch]);
-    const onRemove = useCallback(() => {
-        dispatch(remove());
-    }, [dispatch]);
+    const onInsert = useCallback(
+        (text) => {
+            dispatch(insert(text));
+        },
+        [dispatch]
+    );
+    const onToggle = useCallback(
+        (id) => {
+            dispatch(toggle(id));
+        },
+        [dispatch]
+    );
+    const onRemove = useCallback(
+        (id) => {
+            dispatch(remove(id));
+        },
+        [dispatch]
+    );
+
+    const onChangeInput = useCallback(
+        (input) => {
+            dispatch(changeInput(input));
+        },
+        [dispatch]
+    );
 
     return (
         <TodoComponent
             onInsert={onInsert}
             onRemove={onRemove}
             onToggle={onToggle}
+            onChangeInput={onChangeInput}
             input={input}
             todos={todos}
         />
     );
 };
 
-export default TodoContainer;
+export default React.memo(TodoContainer);
