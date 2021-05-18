7b3a1393ef74aac2c0654c81c1187d55457e1fd4
upship-kim <tkdqook@gmail.com>
2021-05-18 16:38:35 +0900

    coded saga module

--- src/redux-saga/modules/post.js
+++ src/redux-saga/modules/post.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import {call, put, takeEvery} from '@redux-saga/core/effects';
 import {createAction, handleActions} from 'redux-actions';
-
+import * as api from '../lib/api';
 // 액션 정의
 const GET_POST = 'post/GET_POST';
 const GET_POST_SUCCESS = 'post/GET_POST_SUCCESS';
 const GET_POST_FAILURE = 'post/GET_POST_FAILURE';
 
 //액션 함수 정의
-const getPost = createAction(GET_POST);
-const getPostSuccess = createAction(GET_POST_SUCCESS);
-const getPostFailure = createAction(GET_POST_FAILURE);
+const getPost = createAction(GET_POST, (userId) => userId);
+// const getPostSuccess = createAction(GET_POST_SUCCESS);
+// const getPostFailure = createAction(GET_POST_FAILURE);
+
+function* getPostSaga(userId) {
+    yield put(getPost());
+    try {
+        const post = yield call(api.getPost, userId);
+        yield put({type: GET_POST_SUCCESS, payload: post.data});
+    } catch (e) {
+        yield put({type: GET_POST_FAILURE, payload: e});
+    }
+}
+
+export function* postSaga() {
+    yield takeEvery('GET_POST', getPostSaga);
+}
 
 //초기화 값 정의
 const initState = {
@@ -32,11 +47,11 @@ const post = handleActions(
         [GET_POST_FAILURE]: (state, action) => ({
             ...state,
             loading: false,
+            post: action.payload,
             error: true,
         }),
     },
     initState
 );
 
-export {getPost, getPostSuccess, getPostFailure};
 export default post;
